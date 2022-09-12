2e6a977b18c7bf85dc111735dc36a073432233b3
Christian-de-Pedro <cdepedro1998@gmail.com>
2022-09-12 16:18:13 +0200

    Estilos terminados

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import './App.css';
-import Header from './components/Header';
-import Modals from './components/Modals';
-import NavBarCom from './components/NavBar';
-import Ticketing from './components/Ticketing';
+import "./App.css";
+import Header from "./components/Header";
+import NavBarCom from "./components/NavBar";
+import Ticketing from "./components/Ticketing";
 
 function App() {
   return (
-    
     <div className="App">
-    <NavBarCom className="n-div"/>
-    <Header/>
-      
-      <Ticketing/>
-      <Modals/>
+      <NavBarCom className="n-div" />
+      <Header />
+      <Ticketing />
+      {/* <Modal/> */}
     </div>
   );
 }
--- src/components/Ticketing.jsx
+++ src/components/Ticketing.jsx
@@ -1,18 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Popup from "reactjs-popup";
 import image from "../assets/ticketG.svg";
 import image1 from "../assets/ticketR.svg";
-import { useModal } from "./hooks/useModal";
-// import image2 from "../assets/dots.png";
-import Modal from "./Modal";
-
-
+import "./Ticketing.css";
 
 const Ticketing = () => {
-  const [isOpen, openModal, closeModal]= useModal(false);
   const [tickets, setTickets] = useState([]);
-
-  
 
   useEffect(() => {
     axios
@@ -49,10 +43,70 @@ const Ticketing = () => {
             <br />
             {ticket.ticket}
           </div>
-          <div className="col-3">
-            <button onClick={openModal}> ... </button>
-            <Modal isOpen={openModal} closeModal ={closeModal}> </Modal>
-          </div>
+          <Popup
+            trigger={<button className="button"> ... </button>}
+            modal
+            nested
+          >
+            {(close) => (
+              <div class="bg">
+                <div className="modal">
+                  <div class="header">
+                    <div className="picture">
+                      {" "}
+                      {ticket.present === true ? (
+                        <img src={image} alt="ticketGreen" />
+                      ) : (
+                        <img src={image1} alt="ticketRed" />
+                      )}{" "}
+                    </div>
+                    <div className="name">
+                      {" "}
+                      <h4>
+                        {ticket.firstName} {ticket.lastName}
+                      </h4>
+                      <br />
+                      <span>
+                        {" "}
+                        {ticket.present === true
+                          ? "Ha entrado"
+                          : "No ha entrado"}{" "}
+                      </span>
+                    </div>
+                  </div>
+                  <button className="close" onClick={close}>
+                    &times;
+                  </button>
+                  <div>
+                    <div className="body-modal">
+                      <div className="actions">
+                        Fecha de nacimiento
+                        <br />
+                        <h5>{ticket.birthdate}</h5>
+                      </div>
+                      <div className="actions">
+                        Email
+                        <br />
+                        <h5>{ticket.email}</h5>
+                      </div>
+                      <div className="actions">
+                        Telefono
+                        <br />
+                        <h5>{ticket.phone}</h5>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    {" "}
+                    <div className="direction">
+                      <h5> Direccion</h5>
+                      <span> {ticket.address}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </Popup>
         </>
       ))}
     </div>
